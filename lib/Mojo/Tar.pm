a473ba98c1c0fbde5b0700e8aa059dae264f558f
Jan Henning Thorsen <jan.henning@thorsen.pm>
2023-03-13 12:10:00 +0900

    Add looks_like_tar()

--- lib/Mojo/Tar.pm
+++ lib/Mojo/Tar.pm
@@ -3,6 +3,17 @@ use Mojo::Base -base, -signatures;
 
 use Mojo::Tar::File;
 
+use constant TAR_BLOCK_SIZE => 512;
+
 our $VERSION = '0.01';
+
+sub looks_like_tar ($self, $bytes) {
+  state $padding = "\0" x TAR_USTAR_PADDING_LEN;
+  return
+      length($bytes) < TAR_BLOCK_SIZE                                          ? 0
+    : substr($bytes, TAR_USTAR_PADDING_POS, TAR_USTAR_PADDING_LEN) ne $padding ? 0
+    : Mojo::Tar::File->new->from_header($bytes)->checksum                      ? 1
+    :                                                                            0;
+}
 
 1;
--- t/tar.t
+++ t/tar.t
@@ -7,4 +7,16 @@ subtest 'constants' => sub {
   is Mojo::Tar::TAR_USTAR_PADDING_LEN, 12,  'TAR_USTAR_PADDING_LEN';
 };
 
+subtest 'looks_like_tar' => sub {
+  my $tar = Mojo::Tar->new;
+  is $tar->looks_like_tar(''),                              0, 'short';
+  is $tar->looks_like_tar('1' x Mojo::Tar->TAR_BLOCK_SIZE), 0, 'pad missing';
+
+  my $header = Mojo::Tar::File->new->to_header;
+  is $tar->looks_like_tar($header), 1, 'looks like tar';
+
+  substr $header, 0, 3, 'xxx';
+  is $tar->looks_like_tar($header), 0, 'invalid checksum';
+};
+
 done_testing;
